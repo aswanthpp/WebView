1ee765209bab4cc2bf0949e2515ed2c1b8eaa6fc
Your Name Goes Here <ppaswanth3@gmail.com>
2017-05-06 22:47:58 +0530

    First Commit

---
+++ app/src/main/java/com/example/pp/webview/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.pp.webview;
+
+import android.app.ProgressDialog;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    ProgressDialog progressDialog;
+    private static final String TAG = "MainActivity";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressDialog = new ProgressDialog(this);
+
+
+
+        Button facebookid = (Button) findViewById(R.id.facebook);
+        Button youtubeid = (Button) findViewById(R.id.youtube);
+
+
+        facebookid.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("http://www.facebook.com"));
+                startActivity(intent);
+            }
+        });
+
+        youtubeid.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("http://www.youtube.com"));
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
+
